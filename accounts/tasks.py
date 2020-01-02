c99396327220bee3aabff9da25cfd2af0bd9e696
Spotty Banana <spotty.banana@tutanota.com>
2020-01-02 18:09:55 +0100

    implemented basic idea for address fetching

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -3,15 +3,20 @@ from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from BtcTools.local_settings import rpc_user, rpc_password
 
 def BTC_refill_address_queue():
-    # TODO:
-    # connect to the BTC daemon
-    # get addresses via json-rpc command
-    # create address object¨
+    # TODO: move daemon connection string to settings!
     rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%(rpc_user, rpc_password))
+
+    # try to keep always 1000 address objects ready
+    addresses_needed = 1000 - AssetAddress.objects.filter(asset_id=1, account=None).count()
+
+    while i in range(1, addresses_needed):
+        address_string = rpc_connection.getnewaddress()
+        # TODO: validate address, raise Exception if daemon returrns something that is not excepted
+
+
+        AssetAddress.objects.create(address=address_string, account=None)
+
     return rpc_connection.getnewaddress()
-
-
-    pass
 
 
 def refill_address_queue():
