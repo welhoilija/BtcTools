1fd7b4b4885817f59ec4055f06d963b07dea9e6e
welhoilija <tkankas@gmail.com>
2020-01-04 16:20:13 +0200

    updated tasks.py

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -4,11 +4,11 @@ from django.conf import settings
 
 def BTC_refill_address_queue():
     # TODO: move daemon connection string to settings!
-    rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%(settings.RPC_USER, settings.RPC_PASSWORD))
+    daemon_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%(settings.RPC_USER, settings.RPC_PASSWORD))
     # try to keep always 1000 address objects ready
     addresses_needed = 1000 - AssetAddress.objects.filter(asset_id=1, account=None).count()
     for i in range(1, addresses_needed):
-        address_string = rpc_connection.getnewaddress()
+        address_string = daemon_connection.getnewaddress()
         # TODO: validate address, raise Exception if daemon returrns something that is not excepted
         if 26 <= len(address_string) <= 35:
             AssetAddress.objects.create(address=address_string, account=None, asset_id=1)
@@ -26,8 +26,8 @@ def BTC_check_incoming_transactions():
     # TODO:
     # connect to the BTC daemon
     # get latest transactions with listtransactions, listsinceblock or similar
-    rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%(rpc_user, rpc_password))
-    recent_transactions = rpc_connection.listtransactions()
+    daemon_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%(rpc_user, rpc_password))
+    recent_transactions = daemon_connection.listtransactions()
 
     # PSEUDOCODE:
     for tx in recent_transactions:
